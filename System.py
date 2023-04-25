ca821d75301ec5446e02ff1f13debedf7081af7c
gthompso <gthompso@eecs@eecs.wsu.edu>
2023-04-24 23:50:09 -0700

    Added planning for future System plans

--- System.py
+++ System.py
@@ -3,3 +3,5 @@ from BasketballReferenceScraper import *
 # The primary function that is called from main()
 def Run():
     PullData()
+    # Once Data is pulled we should go to Goat calculations to normalize the data to create
+    # standardized values for each accolade
